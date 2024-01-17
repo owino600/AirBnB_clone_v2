a33fa84c561247ec62b7600e649af538b52fd731
owino600 <owinodaniel600@gmail.com>
2024-01-17 13:07:57 +0300

    file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,12 +2,12 @@
 """This is the file storage class for AirBnB."""
 import json
 from models.base_model import BaseModel
-from models.amenity import User
+from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
-from models.user import Amenity
+from models.user import User
 
 
 class FileStorage:
