7d8c54f99fb3e970f5c60dca1873393d21ff09bc
owino600 <owinodaniel600@gmail.com>
2024-01-17 23:58:19 +0300

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from  models import storage
+import models
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
