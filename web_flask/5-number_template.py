3abd4bd1a8afbff716149fe37bd61d459b0f6a98
owino600 <owinodaniel600@gmail.com>
2024-02-16 11:45:22 +0300

    .\5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
-
 The application listens on 0.0.0.0, port 5000.
-Routes:
     /: Displays 'Hello HBNB!'.
     /hbnb: Displays 'HBNB'.
     /c/<text>: Displays 'C' followed by the value of <text>.
